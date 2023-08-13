09bbfcb1159fcc389da16e6f3b7a98cd8dfdf8af
Muddassar Pervaiz <muddassarpervaiz802@gmail.com>
2023-08-13 14:47:02 +0500

    changing

--- MagicVillaAPI/Controllers/VillaAPIController.cs
+++ MagicVillaAPI/Controllers/VillaAPIController.cs
@@ -16,6 +16,7 @@ namespace MagicVillaAPI.Controllers
         //end point on method level not on controller level
         //so IEnumeable<VillaDTO> is get end point  
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]//document the status type 
         public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
             return Ok(VillaStore.villaList);
@@ -24,12 +25,16 @@ namespace MagicVillaAPI.Controllers
         // return villa based on id
         //tell explicitly id is integer
         //[HttpGet("{id:int}")]
-        [HttpGet("id")] //Endpoint
+
+        [HttpGet("id", Name ="GetVilla")] //Endpoint
+
         //[ProducesResponseType(200, Type=typeof(VillaDTO))]    we use this if we want to write method like this
         //ActionResult GetVilla(int id)
-        [ProducesResponseType(200)]//document the status type 
-        [ProducesResponseType(404)]
-        [ProducesResponseType(400)]
+        //[ProducesResponseType(200)] another method to document the status type
+
+        [ProducesResponseType(StatusCodes.Status200OK)]//document the status type 
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public ActionResult<VillaDTO> GetVilla(int id)
         {
             if(id==0)
@@ -42,6 +47,41 @@ namespace MagicVillaAPI.Controllers
                 return NotFound();
             }
             return Ok(villa);
+        }
+
+
+        //Creating Source / or Villa 
+        //For this use HttpPost Method
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]//document the status type 
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+        {
+            //chech villa name, when create villa its does not exist already and create unique name
+            if(VillaStore.villaList.FirstOrDefault(u=>u.Name.ToLower()  == villaDTO.Name.ToLower()) != null)
+            {
+                //in bracket key, value pair
+                ModelState.AddModelError("CustomError", "Villa Name already Exists....");
+                return BadRequest(ModelState);
+            }
+            if (villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+            //if creating villa id should be 0 if id is greate then zero then its means this is not create request
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            //So we retrive maximum id here and increment that by one.
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+
+            //store villaDTO into the villaList
+            VillaStore.villaList.Add(villaDTO);
+
+            //this way is use when create source, you give url where the actual source is created
+            return CreatedAtRoute("GetVilla",new {id = villaDTO.Id  },villaDTO);
         }
     }
 }
--- MagicVillaAPI/Model/Dto/VillaDTO.cs
+++ MagicVillaAPI/Model/Dto/VillaDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace MagicVillaAPI.Model.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVillaAPI.Model.Dto
 {
     public class VillaDTO
     {
         //Dto provide a wrapper between the entity or database model
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
